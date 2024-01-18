258482066dc5e9c48c79b0d3c30be8cb6d4cb1cb
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-01-18 14:30:10 +0200

    Add files via upload

---
+++ APA-Maker.py
@@ -0,0 +1,43 @@
+from SmartConsole import *
+
+class main:
+    # constructor
+    def __init__(self):
+        # load smart console
+        self.sc = SmartConsole("APA Maker", "2.0")
+
+        # set-up main memu
+        self.sc.main_menu["RUN"] = self.run
+
+        # start
+        self.sc.start()
+    
+    def run(self):
+        fname = self.sc.input("Author first name")
+        lname = self.sc.input("Author last name")
+        date = self.sc.input("Publishing date")
+        title = self.sc.input("Article title")
+        webname = self.sc.input("Website name")
+        url = self.sc.input("URL")
+        try:
+            publishername = lname+" "+fname[0]
+            if fname == lname or lname == "":
+                publishername = fname
+            if date == "":
+                date = "n.d."
+            if webname.upper() == "YOUTUBE" or webname.upper() == "YT":
+                title = title+" [Video]"
+            self.sc.print("\nReferences:")
+            self.sc.print("%s. (%s). %s. %s. Retrieved from: %s" % (publishername, date, title, webname, url))
+            self.sc.print("\nIn text citations:")
+            self.sc.print("(%s. %s)" % (publishername, date))
+        except:
+            pass
+        # restart
+        if self.sc.question("\nMake another?"):
+            self.sc.hr()
+            self.run()
+        else:
+            self.sc.exit()
+
+main()
