5de3a70d3e2d6e237129c639061cc88a04629838
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2025-03-13 12:38:33 +0200

    Update APA-Maker.py

---
+++ APA-Maker.py
@@ -0,0 +1,46 @@
+# Download SmartConsole.py from: https://github.com/VladFeldfix/Smart-Console/blob/main/SmartConsole.py
+from SmartConsole import *
+
+class main:
+    # constructor
+    def __init__(self):
+        # load smart console
+        self.sc = SmartConsole("APA Maker", "2.0")
+
+        # set-up main memu
+        self.sc.add_main_menu_item("RUN", self.run)
+
+        # start
+        try:
+            self.sc.start()
+        except Exception as e: print(e)
+    
+    def run(self):
+        fname = self.sc.input("Author first name")
+        lname = self.sc.input("Author last name")
+        date = self.sc.input("Publishing date")
+        title = self.sc.input("Article title")
+        webname = self.sc.input("Website name")
+        url = self.sc.input("URL")
+        try:
+            publishername = lname+" "+fname[0]
+            if fname == lname or lname == "":
+                publishername = fname
+            if date == "":
+                date = "n.d."
+            if webname.upper() == "YOUTUBE" or webname.upper() == "YT":
+                title = title+" [Video]"
+            self.sc.print("\nReferences:")
+            self.sc.print("%s. (%s). %s. %s. Retrieved from: %s" % (publishername, date, title, webname, url))
+            self.sc.print("\nIn text citations:")
+            self.sc.print("(%s. %s)" % (publishername, date))
+        except:
+            pass
+        # restart
+        if self.sc.question("\nMake another?"):
+            self.sc.hr()
+            self.run()
+        else:
+            self.sc.exit()
+
+main()
